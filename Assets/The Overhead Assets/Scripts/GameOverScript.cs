e8a857b22847e2b0f97a8166833007045db3e858
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-05-13 10:56:46 +0600

    Добавил главное меню. Closes #7.

--- Assets/The Overhead Assets/Scripts/GameOverScript.cs
+++ Assets/The Overhead Assets/Scripts/GameOverScript.cs
@@ -1,10 +1,8 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameOverScript : MonoBehaviour {
     public void OnTriggerEnter2D(Collider2D other)
     {
-        int scene = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        GameManager.instance.RestartLevel();
     }
 }
