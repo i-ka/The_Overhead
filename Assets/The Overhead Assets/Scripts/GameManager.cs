1e4fec9affcbd147ce275a3226ddee377b7da4dd
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-05-28 17:12:16 +0600

    Добавил кнопку рестарта. Closes #19.

--- Assets/The Overhead Assets/Scripts/GameManager.cs
+++ Assets/The Overhead Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour {
     private GameObject player;
     private CharacterMoveController playerController;
     private Transform deadMenu;
-
+    private Transform mainMenu;
 
     void Awake()
     {
@@ -21,13 +21,17 @@ public class GameManager : MonoBehaviour {
     }
     void Start()
     {
+        mainMenu = transform.FindChild("MainMenu");
         deadMenu = transform.FindChild("DeadMenu");
+        mainMenu.gameObject.SetActive(true);
+        deadMenu.gameObject.SetActive(false);
     }
 
     public void Setup(GameObject player)
     {
         this.player = player;
         playerController = player.GetComponent<CharacterMoveController>();
+        deadMenu.gameObject.SetActive(false);
     }
 
     void Update()
@@ -36,12 +40,15 @@ public class GameManager : MonoBehaviour {
             if (!playerController.stats.isAlive) {
                 Time.timeScale = 0;
                 deadMenu.gameObject.SetActive(true);
+            } else {
+                Time.timeScale = 1;
             }
         }
     }
 
     public void StartGame()
     {
+        mainMenu.gameObject.SetActive(false);
         SceneManager.LoadScene(1);
     }
     public void Quit()
@@ -50,6 +57,7 @@ public class GameManager : MonoBehaviour {
     }
     public void RestartLevel()
     {
+        deadMenu.gameObject.SetActive(false);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
