ae4962a360cd2f0539be32e816d06fad6a539661
aaptuhin <ptuhinsasha@mail.ru>
2016-05-29 22:17:00 +0500

    add player health ui closes #18

--- Assets/The Overhead Assets/Scripts/StatManager.cs
+++ Assets/The Overhead Assets/Scripts/StatManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StatManager : MonoBehaviour {
 
@@ -7,12 +8,20 @@ public class StatManager : MonoBehaviour {
     public int health=100;
     public float attackCoolDown = 1.0f;
     public float pushBackForce = 100;
+    public Slider healthSlider;
     [HideInInspector]
     public bool isAlive = true;
+
+    void Start()
+    {
+        healthSlider.maxValue = health;
+        healthSlider.value = health;
+    }
 
     public void takeDamage(int damage)
     {
         health -= damage;
+        healthSlider.value = health;
         if (health <= 0) {
             isAlive = false;
         }
