e5e46306deff04efd3245fefc97467a8ec1d5c55
aaptuhin <ptuhinsasha@mail.ru>
2016-05-29 21:54:44 +0500

    added thorns that cause damage and push away closes #17

---
+++ Assets/ThornsDamage.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThornsDamage : MonoBehaviour
+{
+    public int damage;
+    public int pushBackForce;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Enemy"))
+        {
+            other.SendMessageUpwards("ApplyDamage", damage);
+            Vector2 pushVector = new Vector2(other.transform.position.x - transform.position.x, 20);
+            other.GetComponent<CharacterMoveController>().pushBack(pushVector.normalized * pushBackForce);
+        }
+    }
+}
