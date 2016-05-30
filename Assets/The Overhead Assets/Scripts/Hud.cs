3d0b47abd7aeaa110239985faa3c2bd5dd5db438
ФО-240003 Калинич Игнат Владимирович <i-ka2010@mail.ru>
2016-05-30 15:33:49 +0600

    Вынес скрипт хп бара в компонент игрока.

---
+++ Assets/The Overhead Assets/Scripts/Hud.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour {
+
+    [SerializeField]
+    private Slider hpBar;
+
+	// Use this for initialization
+	void Start () {
+        hpBar.maxValue = hpBar.value = GetComponent<StatManager>().health;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        hpBar.value = Mathf.Lerp(hpBar.value, GetComponent<StatManager>().health, Time.deltaTime * 10);
+	}
+}
--- Assets/The Overhead Assets/Scripts/StatManager.cs
+++ Assets/The Overhead Assets/Scripts/StatManager.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 public class StatManager : MonoBehaviour {
 
@@ -8,20 +7,16 @@ public class StatManager : MonoBehaviour {
     public int health=100;
     public float attackCoolDown = 1.0f;
     public float pushBackForce = 100;
-    public Slider healthSlider;
     [HideInInspector]
     public bool isAlive = true;
 
     void Start()
     {
-        healthSlider.maxValue = health;
-        healthSlider.value = health;
     }
 
     public void takeDamage(int damage)
     {
         health -= damage;
-        healthSlider.value = health;
         if (health <= 0) {
             isAlive = false;
         }
